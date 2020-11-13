c95d6797d6f6cd2f505923861b016a1807fb8902
P3ntest <juliusvanvoorden@outlook.com>
2020-11-13 20:28:49 +0100

    add warn command

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import { Client, Message, MessageEmbed } from "discord.js";
+import { Client, DMChannel, GuildMember, Message, MessageEmbed, MessageFlags, TextChannel } from "discord.js";
 import * as YAML from "yamljs";
 import path from "path";
-import { copyFile } from "fs";
+import arg from "arg";
+import { sign } from "crypto";
 
 const client = new Client();
 const config = YAML.load(path.resolve(__dirname, "config.yaml"));
@@ -13,7 +14,7 @@ client.on("ready", () => {
 client.on("message", message => {
     if (message.content.startsWith(config.bot.prefix)) {
         let command = message.content.split(" ")[0].substr(config.bot.prefix.length);
-        let args = message.content.substr(command.length + config.bot.prefix.length);
+        let args = message.cleanContent.substr(command.length + config.bot.prefix.length);
 
         switch (command.toLocaleLowerCase()) {
             case "cmdecho":
@@ -22,9 +23,56 @@ client.on("message", message => {
                 .setDescription("Echos the typed command back.")
                 .setColor("#7c76e0")
                 .addField("\u200b", "\u200b")
+                .addField("Message Content", message.cleanContent)
                 .addField("Command", command ? command : "", true)
                 .addField("Arguments", args ? args : "n.a.", true));
                 break;
+            case "warn":
+                if (!config.ids.owners.includes(message.author.id))
+                    return;
+                if (message.channel instanceof TextChannel) {
+                    let parsed = arg({
+                        "-c": String, //The channel
+                        "-m": String, //The message
+                        "-h": Boolean, //Hide who warned
+                    }, {permissive: true, argv: args.split(" ")});
+                    if (message.mentions.members?.size == 0) {
+                        message.reply(new MessageEmbed().setColor("#ff0000").setDescription("Please specify a Member!"));
+                        return;
+                    }
+
+                    if (message.mentions.members?.first(1)[0] == null) {
+                        message.reply(new MessageEmbed().setColor("#ff0000").setDescription("An internal error occured."));
+                        return;
+                    }
+
+                    let warnedMember: GuildMember | null = message.mentions.members?.first(1)[0];
+
+                    let embed = new MessageEmbed().setColor("#7c76e0").setTitle("\u200b").setTimestamp();
+
+                    let channel: TextChannel;
+
+                    embed.setAuthor(warnedMember.user.tag + " has been warned", warnedMember.user.avatarURL() ?? "https://moonvillageassociation.org/wp-content/uploads/2018/06/default-profile-picture1.jpg");
+                    
+                    embed.addField("Reason", parsed["-m"] ? parsed["-m"].replace("_", " ") : "Inappropiate Behavior", true);
+
+                    if (!parsed["-h"])
+                        embed.setFooter(message.author.tag, message.author.avatarURL() ?? "");
+
+                    embed.addField("Level", ":a:", true);
+
+                    if (parsed["-c"])
+                        channel = message.mentions.channels.first(1)[0];
+                    else
+                        channel = message.channel;
+
+                    channel.send(embed);
+
+                    message.delete();
+
+                } else {
+                    message.reply(new MessageEmbed().setColor("#ff0000").setDescription("This only works in servers."));
+                }
             case "":
                 break;
             default:
