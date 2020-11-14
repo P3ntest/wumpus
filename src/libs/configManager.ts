14eb24dd9041fba5512f189216356693ad73a84d
P3ntest <juliusvanvoorden@outlook.com>
2020-11-14 09:51:48 +0100

    fix double return error in configManager

--- src/libs/configManager.ts
+++ src/libs/configManager.ts
@@ -4,19 +4,19 @@ import * as YAML from "yamljs";
 import fs from "fs";
 
 
-export function loadConfig() {
+export function loadConfig() {    
     let pathToDistConfig = path.join(__dirname, "../config.yaml");
 
     if (!fs.existsSync(pathToDistConfig)) {
         fs.copyFileSync(path.join(__dirname, "../../src/config.yaml"), pathToDistConfig);
     }
 
-    let config = YAML.load(path.resolve(__dirname, "config.yaml"));
+    let config = YAML.load(pathToDistConfig);
 
     if (config.bot.token.length < 10) {
         console.error("No Token set in ./dist/config.yaml. Exiting");
         process.exit(1);
     }
 
-    return YAML.load(path.resolve(__dirname, "config.yaml"));
+    return config;
 }
